23ea885bb8e78617fb2d8259d7ce2620ed3b4c92
Riddhi Suteri <ridsuteri@gmail.com>
2022-06-24 08:58:43 +0530

    configured alan ai keys

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React,{useEffect} from "react"
 import { css } from "emotion"
 import "./layout.css"
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    alanBtn({
+        key: '',
+        onCommand: (commandData) => {
+          if (commandData.command === 'nextJoke') {
+            console.log('Next Joke');
+          }
+        }
+    });
+  }, []);
   return (
     <main
       className={css`
